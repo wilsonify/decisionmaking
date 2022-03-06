c8f735d8c2dbde80d0b16e3c043dbbc2cf6d6607
Tom Wilson <tom.andrew.wilson@gmail.com>
2022-03-06 12:37:30 -0500

    add structutre learning

---
+++ src/I-Probabilistic-Reasoning/06-Simple-Decisions/Value-of-Information.jl
@@ -0,0 +1,21 @@
+function value_of_information(𝒫, query, evidence, M)
+    """
+    A method for de        cision network evaluation, which
+        takes a simple problem 𝒫, a list of
+        query variables query, a dictionary
+        containing observed chance vari        ables and their values evidence,
+        and an inference strategy M. The
+        method returns an assignment of
+        decision values that maximizes
+        the expected utility, given the evi        dence.
+    """
+    φ = infer(M, 𝒫.bn, query, evidence)
+    voi = -solve(𝒫, evidence, M).u
+    query_vars = filter(v -> v.name ∈ query, 𝒫.chance_vars)
+    for o′ in assignments(query_vars)
+        oo′ = merge(evidence, o′)
+        p = φ.table[o′]
+        voi += p * solve(𝒫, oo′, M).u
+    end
+    return voi
+end
