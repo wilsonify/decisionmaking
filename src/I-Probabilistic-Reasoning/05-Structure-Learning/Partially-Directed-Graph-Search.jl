c5918610244cb569f002fd04ed49e08d31c09a1f
wilsonify <tom.a.wilson@gmail.com>
2022-03-07 07:53:15 -0500

    add exact-solution-methods

--- src/I-Probabilistic-Reasoning/05-Structure-Learning/Partially-Directed-Graph-Search.jl
+++ src/I-Probabilistic-Reasoning/05-Structure-Learning/Partially-Directed-Graph-Search.jl
@@ -6,21 +6,22 @@ function are_markov_equivalent(G, H)
         from IterTools.jl returns all sub        sets of a given set and a specified
         size.
     """
-    if nv(G) != nv(H) || ne(G) != ne(H) ||
-    !all(has_edge(H, e) || has_edge(H, reverse(e))
-    for e in edges(G))
-    return false
+    if nv(G) != nv(H) ||
+       ne(G) != ne(H) ||
+       !all(has_edge(H, e) || has_edge(H, reverse(e)) for e in edges(G))
+        return false
     end
-    for (I, J) in [(G,H), (H,G)]
-    for c in 1:nv(I)
-    parents = inneighbors(I, c)
-    for (a, b) in subsets(parents, 2)
-    if !has_edge(I, a, b) && !has_edge(I, b, a) &&
-    !(has_edge(J, a, c) && has_edge(J, b, c))
-    return false
-    end
-    end
-    end
+    for (I, J) in [(G, H), (H, G)]
+        for c = 1:nv(I)
+            parents = inneighbors(I, c)
+            for (a, b) in subsets(parents, 2)
+                if !has_edge(I, a, b) &&
+                   !has_edge(I, b, a) &&
+                   !(has_edge(J, a, c) && has_edge(J, b, c))
+                    return false
+                end
+            end
+        end
     end
     return true
-    end
+end
