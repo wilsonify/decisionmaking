bca79fb0a71c7728e30a761b3605dce8e2c1aad4
wilsonify <tom.a.wilson@gmail.com>
2022-03-09 07:36:48 -0500

    add Policy-Gradient-Optimization

---
+++ src/II-Sequential-Problems/12-Policy-Gradient-Optimization/Gradient Ascent Update.jl
@@ -0,0 +1,29 @@
+struct PolicyGradientUpdate
+    """
+    The gradient as-
+cent method for policy optimiza-
+tion. It takes a step from a point
+θ in the direction of the gradient
+∇U with step factor α . We can use
+one of the methods in the previous
+chapter to compute ∇U .
+    """
+    ∇U::Any # policy gradient estimate
+    α::Any # step factor
+end
+function update(M::PolicyGradientUpdate, θ)
+    return θ + M.α * M.∇U(θ)
+end
+
+"""
+Methods for gra-
+    dient scaling and clipping. Gradi-
+    ent scaling limits the magnitude
+    of the provided gradient vector ∇
+    to L2_max . Gradient clipping pro-
+    vides elementwise clamping of the
+    provided gradient vector ∇ to be-
+    tween a and b .
+"""
+scale_gradient(∇, L2_max) = min(L2_max / norm(∇), 1) * ∇
+clip_gradient(∇, a, b) = clamp.(∇, a, b)
