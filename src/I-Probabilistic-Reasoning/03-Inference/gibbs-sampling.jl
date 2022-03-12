f2b465beb3fa58354dc1f73f5a4c39870fb57b8c
wilsonify <tom.a.wilson@gmail.com>
2022-03-12 18:31:32 -0500

    fix comments

--- src/I-Probabilistic-Reasoning/03-Inference/gibbs-sampling.jl
+++ src/I-Probabilistic-Reasoning/03-Inference/gibbs-sampling.jl
@@ -16,41 +16,41 @@ function update_gibbs_sample!(a, bn, evidence, ordering)
     """
     Gibbs sampling implemented for a Bayesian network bn with evidence evidence and an ordering ordering.
     The method iteratively updates the assignment a for m iterations.
+    to use
+    to discard during burn-in
+    to skip for thinning indices    
     """
     for i in ordering
-    name = bn.vars[i].name
-    if !haskey(evidence, name)
-    b = blanket(bn, a, i)
-    a[name] = rand(b)[name]
+        name = bn.vars[i].name
+        if !haskey(evidence, name)
+            b = blanket(bn, a, i)
+            a[name] = rand(b)[name]
+        end
     end
-    end
-    end
-    
-    function gibbs_sample!(a, bn, evidence, ordering, m)
+end
+
+function gibbs_sample!(a, bn, evidence, ordering, m)
     for j in 1:m
-    update_gibbs_sample!(a, bn, evidence, ordering)
+        update_gibbs_sample!(a, bn, evidence, ordering)
     end
-    end
-    struct GibbsSampling
+end
+struct GibbsSampling
     m_samples # number of samples
     m_burnin # number of samples
     m_skip
     # number of samples
     ordering # array of variable
-    end
-    to use
-    to discard during burn-in
-    to skip for thinning
-    indices
-    function infer(M::GibbsSampling, bn, query, evidence)
+end
+
+function infer(M::GibbsSampling, bn, query, evidence)
     table = FactorTable()
     a = merge(rand(bn), evidence)
     gibbs_sample!(a, bn, evidence, M.ordering, M.m_burnin)
     for i in 1:(M.m_samples)
-    gibbs_sample!(a, bn, evidence, M.ordering, M.m_skip)
-    b = select(a, query)
-    table[b] = get(table, b, 0) + 1
+        gibbs_sample!(a, bn, evidence, M.ordering, M.m_skip)
+        b = select(a, query)
+        table[b] = get(table, b, 0) + 1
     end
-    vars = filter(v->v.name ∈ query, bn.vars)
+    vars = filter(v -> v.name ∈ query, bn.vars)
     return normalize!(Factor(vars, table))
-    end
+end
