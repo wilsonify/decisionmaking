c7f68e5a56499d56a01b36e49cd06ff724b2f3bb
wilsonify <tom.a.wilson@gmail.com>
2022-03-20 08:48:18 -0400

    add tests to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,25 @@ using DecisionMakingAlgorithms
 using Test
 
 
-
 @testset "DecisionMakingAlgorithms.jl" begin
+    @time @safetestset "01-Introduction Tests" begin
+        include("I-Probabilistic-Reasoning/01-Introduction/test_test.jl")
+    end
     
-@time @safetestset "Introduction Tests" begin include("I-Probabilistic-Reasoning/01-Introduction/test_test.jl") end
-#@time @safetestset "Tstops Tests" begin include("interface/ode_tstops_tests.jl") end
-#@time @safetestset "Backwards Tests" begin include("interface/ode_backwards_test.jl") end
-#@time @safetestset "Initdt Tests" begin include("interface/ode_initdt_tests.jl") end
-#@time @safetestset "Linear Tests" begin include("interface/ode_twodimlinear_tests.jl") end
-#@time @safetestset "Mass Matrix Tests" begin include("interface/mass_matrix_tests.jl") end
-#@time @safetestset "Differentiation Trait Tests" begin include("interface/differentiation_traits_tests.jl") end
-#@time @safetestset "Inf Tests" begin include("interface/inf_handling.jl") end
+    @time @safetestset "02-Representation Tests" begin
+        include("I-Probabilistic-Reasoning/02-Representation/test-bayesian-networks.jl")
+    end
+
+    @time @safetestset "03-Inference Tests" begin
+        include("I-Probabilistic-Reasoning/03-Inference/test-inference.jl")
+    end
+
+    @time @safetestset "03-Inference Tests" begin
+        include("I-Probabilistic-Reasoning/03-Inference/test-gaussia-model-inference.jl")
+    end
+
+    @time @safetestset "05-Structure-Learning Tests" begin
+        include("I-Probabilistic-Reasoning/05-Structure-Learning/test-bayesian-network-scoring.jl")
+    end
 
 end
