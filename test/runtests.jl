629d48ba5306306e511be6d8f00d0584d1822429
wilsonify <tom.a.wilson@gmail.com>
2022-03-25 07:31:35 -0400

    add tests to runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,10 @@ using SafeTestsets
 @testset "DecisionMakingAlgorithms.jl" begin
     @time @safetestset "01-Introduction Tests" begin
         path_to_here=@__DIR__
-        include("$path_to_here/I-Probabilistic-Reasoning/01-Introduction/test_test.jl")
+        include("$path_to_here/I-Probabilistic-Reasoning/01-Introduction/test-control-flow.jl")
+        include("$path_to_here/I-Probabilistic-Reasoning/01-Introduction/test-functions.jl")
+        include("$path_to_here/I-Probabilistic-Reasoning/01-Introduction/test-packages.jl")
+        include("$path_to_here/I-Probabilistic-Reasoning/01-Introduction/test-types.jl")
     end
     
     @time @safetestset "02-Representation Tests" begin
