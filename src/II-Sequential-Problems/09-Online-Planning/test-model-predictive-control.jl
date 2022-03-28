93e47536dec122708ced59c2773bc72d357bec3f
wilsonify <tom.a.wilson@gmail.com>
2022-03-28 07:28:26 -0400

    copy examples to tests

---
+++ src/II-Sequential-Problems/09-Online-Planning/test-model-predictive-control.jl
@@ -0,0 +1,30 @@
+using Ipopt
+
+"""
+An example of open-loop planning in a deterministic environment. 
+We attempt to find a path around a circular obstacle. 
+This implementation uses the JuMP.jl interface to the Ipopt solver. 
+
+A. Wächter and L. T. Biegler, “On the Implementation of an Interior-Point Filter Line-Search Algorithm for Large-Scale Nonlinear Programming,” Mathematical Programming,
+vol. 106, no. 1, pp. 25-57, 2005.
+"""
+model = Model(Ipopt.Optimizer)
+d = 10
+current_state = zeros(4)
+goal = [10,10,0,0]
+obstacle = [3,4]
+@variables model begin
+s[1:4, 1:d]
+-1 ≤ a[1:2,1:d] ≤ 1
+end
+# velocity update
+@constraint(model, [i=2:d,j=1:2], s[2+j,i] == s[2+j,i-1] + a[j,i-1])
+# position update
+@constraint(model, [i=2:d,j=1:2], s[j,i] == s[j,i-1] + s[2+j,i-1])
+# initial condition
+@constraint(model, s[:,1] .== current_state)
+# obstacle
+@constraint(model, [i=1:d], sum((s[1:2,i] - obstacle).^2) ≥ 4)
+@objective(model, Min, 100*sum((s[:,d] - goal).^2) + sum(a.^2))
+optimize!(model)
+action = value.(a[:,1])
